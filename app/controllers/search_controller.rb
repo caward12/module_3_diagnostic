c8f1c5000cc152304307cd3a83ab052468c336cb
caward12 <ward.colleen.a@gmail.com>
2017-05-17 11:15:28 -0600

    all tests passing and refactored

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,6 @@
 class SearchController < ApplicationController
 
   def index
-    response = Faraday.get "http://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?location=#{params[:q]}&radius=6.0&fuel_types=ELEC,LPG&limit=10&api_key=#{ENV['NREL_KEY']}"
-    parsed = JSON.parse(response.body, symbolize_names: true)
-    @stations = parsed[:fuel_stations]
+    @stations = Station.stations(ENV['NREL_KEY'], params[:q])
   end
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,12 +1,35 @@
 class Station
 
+  attr_reader :attrs
+
   def initialize(attrs={})
     @attrs = attrs
   end
 
   def self.stations(key, location)
-    NrelService.new(key).find_stations(location).each do |data|
-      self.new(data)
+
+    NrelService.new(key).find_stations(location).map do |data|
+      new(data)
     end
+  end
+
+  def station_name
+    attrs[:station_name]
+  end
+
+  def address
+    "#{attrs[:street_address]} #{attrs[:city]}, #{attrs[:state]} #{attrs[:zip]}"
+  end
+
+  def fuel_type
+    attrs[:fuel_type_code]
+  end
+
+  def distance
+    attrs[:distance].round(2)
+  end
+
+  def acces_times
+    attrs[:access_days_time]
   end
 end
