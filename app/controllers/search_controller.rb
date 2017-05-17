3f3adec6c40bfbd34002c24290bdae9ee0626f01
caward12 <ward.colleen.a@gmail.com>
2017-05-17 10:15:54 -0600

    feature test mostly passing, with a few tweaks still needed to format

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,8 @@
 class SearchController < ApplicationController
 
   def index
-    response = Faraday.get "http://api.data.gov/nrel/alt-fuel-stations/v1"
+    response = Faraday.get "http://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?location=#{params[:q]}&radius=6.0&fuel_types=ELEC,LPG&limit=10&api_key=#{ENV['NREL_KEY']}"
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    @stations = parsed[:fuel_stations]
   end
 end
